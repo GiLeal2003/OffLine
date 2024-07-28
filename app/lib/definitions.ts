aea748e252c6d054417f51a162acd8cf25308d64
GiLeal2003 <gileal2003@outlook.com>
2024-07-28 20:12:15 -0300

    Commit

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -6,115 +6,96 @@
 
 //Tabelas
 export type User = {
-  cod: number;
-  nome: string;
+  id: string;
+  name: string;
+  email: string;
+  password: string;
 };
 
-export type Cliente = {
-  cod: number;
-  nome: string;
+export type Customer = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
 };
 
-export type Estabelecimento = {
-  cod: number;
-  nome: string;
+export type Invoice = {
+  id: string;
+  customer_id: string;
+  amount: number;
+  date: string;
+  // In TypeScript, this is called a string union type.
+  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
+  status: 'pending' | 'paid';
 };
 
-export type Item = {
-  cod: string;
-  nome: string;
-  preco: number;
-  tipo: string;
-  descricao: string;
-  cod_estabelecimento: number;
+/*
+export type Invoice = {
+  id: string; // Will be created on the database
+  customer_id: string;
+  amount: number; // Stored in cents
+  status: 'pending' | 'paid';
+  date: string;
 };
+*/
 
-export type Pedido = {
-  cod: number;
-  datas: string;
-  statusPedido:  'Em andamento' | 'Cancelado' | 'Finalizado';
-  tipo_pagamento: string;
-  valor_total: number;
-  cod_cliente: number;
-  cod_estabelecimento: number;
-
-};
-
-export type Item_Pedido = {
-  cod_pedido: number;
-  cod_item: number;
-  cod_estabelecimento: number;
-  quantidade_item: number;
-
-};
-
-//Funções para os códigos
 export type Revenue = {
-  mes: string;
+  month: string;
   revenue: number;
 };
 
-export type UltimosPedidos = {
-  cod: number;
-  nome: string;
-  imagem: string;
-  datas: string;
-  valor_total: number;
-  cod_cliente: number;
-  cod_estabelecimento: number;
+export type LatestInvoice = {
+  id: string;
+  name: string;
+  image_url: string;
+  email: string;
+  amount: string;
 };
 
-export type PedidosAtualizados = Omit<UltimosPedidos, 'valor_total'> & {
-  valor_total: number;
+// The database returns a number for amount, but we later format it to a string with the formatCurrency function
+export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
+  amount: number;
 };
 
-export type TabelaPedidos = {
-  cod: number;
-  nome: string;
-  datas: string;
-  statusPedido:  'Em andamento' | 'Cancelado' | 'Finalizado';
-  tipo_pagamento: string;
-  valor_total: number;
-  cod_cliente: number;
-  cod_estabelecimento: number;
-  imagens: string;
+export type InvoicesTable = {
+  id: string;
+  customer_id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  date: string;
+  amount: number;
+  status: 'pending' | 'paid';
 };
 
-export type TabelaClientes = {
-  cod: number;
-  nome: string;
-  datas: string;
-  statusPedido:  'Em andamento' | 'Cancelado' | 'Finalizado';
-  tipo_pagamento: string;
-  valor_total: number;
-  cod_cliente: number;
-  cod_estabelecimento: number;
-  imagens: string;
-  total_pedidos: number;
-
+export type CustomersTableType = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  total_invoices: number;
+  total_pending: number;
+  total_paid: number;
 };
 
-export type TabelaClientesFormatted = {
-  cod: number;
-  nome: string;
-  datas: string;
-  statusPedido:  'Em andamento' | 'Cancelado' | 'Finalizado';
-  tipo_pagamento: string;
-  valor_total: number;
-  cod_cliente: number;
-  cod_estabelecimento: number;
-  imagens: string;
-  total_pedidos: number;
+export type FormattedCustomersTable = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  total_invoices: number;
+  total_pending: string;
+  total_paid: string;
 };
 
-export type FieldClientes = {
-  cod: number;
-  nome: string;
+export type CustomerField = {
+  id: string;
+  name: string;
 };
 
-export type FormPedidos = {
-  cod: string;
-  cod_cliente: string;
-  valor_total: number;
-  statusPedido:  'Em andamento' | 'Cancelado' | 'Finalizado';
+export type InvoiceForm = {
+  id: string;
+  customer_id: string;
+  amount: number;
+  status: 'pending' | 'paid';
 };
