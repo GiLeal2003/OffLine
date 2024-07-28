710f7f12712e5bd9def7fc440a9420db6a6b1693
GiLeal2003 <gileal2003@outlook.com>
2024-07-28 16:02:14 -0300

    Commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-//module.exports = nextConfig;
-module.exports =  { eslint: { ignoreDuringBuilds: true } };
+module.exports = nextConfig;
